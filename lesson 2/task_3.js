db4cba88adb2603383d7489a3e041645d1380228
dima0208 <mashenko0208@gmail.com>
2022-07-30 14:50:02 +0300

    first commit

---
+++ lesson 2/task_3.js
@@ -0,0 +1,10 @@
+let numFive = prompt(`Давай посчитаем среднее арифметическое 3 чисел.
+
+Введите первое число`);
+
+let numSix = prompt('Введите второе число');
+let numSeven = prompt('Введите третье число');
+
+console.log(' ');
+console.log('=====Задание 3=====');
+console.log(`Среднее арифметическое трех чисел: ${(+numFive + +numSix + +numSeven) / 3}`);
---
+++ lesson 2/task_4.js
@@ -0,0 +1,10 @@
+let strOne = prompt(`Давай попробуем вывести фразу из трех слов и получить ее в консоль.
+
+Введите первое слово`);
+
+let strTwo = prompt('Введите второе слово');
+let strThree = prompt('Введите третье слово');
+
+console.log(' ');
+console.log('=====Задание 4=====');
+console.log('Фраза: ' + strOne + ' ' + strTwo + ' ' + strThree);

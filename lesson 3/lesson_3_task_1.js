afaf269281be68f561068ea77b65cd85e9500ac4
dima0208 <mashenko0208@gmail.com>
2022-08-28 18:34:11 +0300

    hw n2, rev 2

--- lesson 3/lesson_3_task_1.js
+++ lesson 3/lesson_3_task_1.js
@@ -1,18 +1,13 @@
-const numOne = prompt(
-    `Давай введем 3 числа и узнаем, сколько из них одинаковые.
-    
-Введите первое число`
-);
+const numOne = prompt('Введите трехзначное число');
 
-const numTwo = prompt('Введите второе число');
-const numThree = prompt('Введите третье число');
-
-console.log('=====Task 1=====');
-
-if (numOne == numTwo && numTwo == numThree) {
-    console.log(`В числе ${numOne + numTwo + numThree} все цифры цифры`)
-} else if (numOne == numTwo || numTwo == numThree || numOne == numThree) {
-    console.log(`В числе ${numOne + numTwo + numThree}  две цифры одинаковые`)
+if (numOne[0] == numOne[1] && numOne[1] == numOne[2]) {
+  console.log(`В числе ${numOne} все цифры равны`);
+} else if (
+  numOne[0] == numOne[1] ||
+  numOne[1] == numOne[2] ||
+  numOne[0] == numOne[2]
+) {
+  console.log(`В числе ${numOne} две цифры одинаковые`);
 } else {
-    console.log(`В числе ${numOne + numTwo + numThree} все цифры разные`)
-};
+  console.log(`В числе ${numOne} все цифры разные`);
+}
--- lesson 3/lesson_3_task_3.js
+++ lesson 3/lesson_3_task_3.js
@@ -1,5 +1,5 @@
 const numFive = prompt(
-    `Введите 3 числа, а я определю, какое из них наиболшее
+  `Введите 3 числа, а я определю, какое из них наиболшее
     
 Введите первое число`
 );
@@ -10,8 +10,10 @@ const numSeven = prompt('Введите третье число');
 console.log(' ');
 console.log('=====Task 3=====');
 
-if (+numFive > +numSix && +numFive > +numSeven) {
-    console.log(`Наиболшее число: ${numFive}`)
-} else if (+numSix > +numFive && +numSix > +numSeven) {
-    console.log(`Наиболшее число: ${numSix}`)
-} else {console.log(`Наиболшее число: ${numSeven}`)};
+if (+numFive >= +numSix && +numFive >= +numSeven) {
+  console.log(`Наиболшее число: ${numFive}`);
+} else if (+numSix >= +numFive && +numSix >= +numSeven) {
+  console.log(`Наиболшее число: ${numSix}`);
+} else {
+  console.log(`Наиболшее число: ${numSeven}`);
+}

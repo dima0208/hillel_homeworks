f7a0c6621828807fc9862cf98886b3c6d6b7f854
dima0208 <mashenko0208@gmail.com>
2022-09-17 18:25:27 +0300

    hw 14 rev. 1.2

--- lesson_10/index.js
+++ lesson_10/index.js
@@ -14,50 +14,86 @@ const images = [
 ];
 
 const buttonContainer = document.querySelector('.slider__button-container');
-// еще привыкаю к переносам prettier
+const progressBar = document.querySelector('.slider__progressbar');
 let number = 0;
-buttonContainer
-  .closest('.slider__wrapper')
-  .querySelector('.slider__progress-text').innerHTML = `Фото ${number + 1} из ${
-  images.length
-}`;
+
+for (let i = 0; i < images.length; i++) {
+  const progressPoint = document.createElement('span');
+  progressPoint.setAttribute('data-id', [i]);
+  progressBar.append(progressPoint);
+}
+const pointsArray = document.querySelectorAll('.slider__progressbar span');
+pointsArray[0].classList.add('_active');
+
+function changeProgressbar() {
+  pointsArray.forEach(function (item) {
+    item.classList.remove('_active');
+  });
+  pointsArray[number].classList.add('_active');
+}
 
 buttonContainer.addEventListener('click', function (event) {
   if (event.target.classList.contains('slider__prev-button')) {
     this.closest('.slider__wrapper')
       .querySelector('.slider__image')
       .setAttribute('src', images[--number]);
+    changeProgressbar();
   }
   if (event.target.classList.contains('slider__next-button')) {
     this.closest('.slider__wrapper')
       .querySelector('.slider__image')
       .setAttribute('src', images[++number]);
+    changeProgressbar();
+  }
+  if (event.target.getAttribute('data-id')) {
+    number = event.target.getAttribute('data-id');
+    this.closest('.slider__wrapper')
+      .querySelector('.slider__image')
+      .setAttribute('src', images[number]);
+    changeProgressbar();
   }
 
-  let lockedButton = () => {
-    if (number < 1) {
-      this.closest('.slider__wrapper').querySelector(
-        '.slider__prev-button'
-      ).disabled = true;
-    } else {
-      this.closest('.slider__wrapper').querySelector(
-        '.slider__prev-button'
-      ).disabled = false;
-    }
-    if (number == images.length - 1) {
-      this.closest('.slider__wrapper').querySelector(
-        '.slider__next-button'
-      ).disabled = true;
-    } else {
-      this.closest('.slider__wrapper').querySelector(
-        '.slider__next-button'
-      ).disabled = false;
-    }
-  };
-  lockedButton();
-  buttonContainer
-    .closest('.slider__wrapper')
-    .querySelector('.slider__progress-text').innerHTML = `Фото ${
-    number + 1
-  } из ${images.length}`;
+  if (number < 1) {
+    this.closest('.slider__wrapper').querySelector(
+      '.slider__prev-button'
+    ).disabled = true;
+  } else {
+    this.closest('.slider__wrapper').querySelector(
+      '.slider__prev-button'
+    ).disabled = false;
+  }
+  if (number > images.length - 2) {
+    this.closest('.slider__wrapper').querySelector(
+      '.slider__next-button'
+    ).disabled = true;
+  } else {
+    this.closest('.slider__wrapper').querySelector(
+      '.slider__next-button'
+    ).disabled = false;
+  }
+  clearInterval(timer1); // остановка таймера по нажатию на кнопку prev/next
 });
+
+// -----------------------------Part 2 Autoscroll----------------------------------
+
+const domImage = document.querySelector('.slider__image');
+const prevButton = document.querySelector('.slider__prev-button');
+const nextButton = document.querySelector('.slider__next-button');
+
+const autoScroll = () => {
+  domImage.setAttribute('src', images[++number]);
+  changeProgressbar();
+  if (number < 1) {
+    prevButton.disabled = true;
+  } else {
+    prevButton.disabled = false;
+  }
+  if (number === images.length - 1) {
+    clearInterval(timer1);
+    nextButton.disabled = true;
+  } else {
+    nextButton.disabled = false;
+  }
+};
+
+const timer1 = setInterval(autoScroll, 2000);

25020e51d8523bcbf8f8a9c5d6de38469f59b9a9
dima0208 <mashenko0208@gmail.com>
2022-10-01 15:10:11 +0300

    hw 17
    slider swiper

---
+++ lesson_18/index.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  effect: 'cube',
+  speed: 600,
+});

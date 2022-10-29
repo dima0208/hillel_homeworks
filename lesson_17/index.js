9bdcd860ade420044be74c15e846750e8e0603d4
dima0208 <mashenko0208@gmail.com>
2022-10-29 17:03:21 +0200

    fixed_hw-16

--- lesson_17/index.js
+++ lesson_17/index.js
@@ -1,14 +1,14 @@
 'use strict';
 
-let flag = true;
-let $firstWidth = $(window).width();
-$(window).on('resize', function () {
-  const $window = $(window).width();
-  if (flag === true && $window - $firstWidth < 0 && $window <= 768) {
+let flag;
+
+$(window).on('resize', () => {
+  const $windowWidth = $(window).width();
+
+  if (flag && $windowWidth <= 768) {
+    $('#exampleModal').modal('show');
     flag = false;
-    $('.btn').trigger('click');
-  } else if ($window > 768) {
+  } else if ($windowWidth > 768) {
     flag = true;
   }
-  $firstWidth = $window;
 });
